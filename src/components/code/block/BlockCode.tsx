fe9e1157e09b08a6cdb41a16e0a1278257d48233
0fprod <franp.dev@pm.me>
2023-06-19 22:54:21 +0100

    set static css for light-dark  themes

--- src/components/code/block/BlockCode.tsx
+++ src/components/code/block/BlockCode.tsx
@@ -1,6 +1,5 @@
-import { oneDark, oneLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import React, { useEffect, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
 interface CodeBlockProps {
   className?: string;
@@ -8,18 +7,19 @@ interface CodeBlockProps {
 }
 
 export const CodeBlock = ({ className, content }: CodeBlockProps) => {
-  const [mounted, setMounted] = React.useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  const theme = mounted && window.matchMedia('(prefers-color-scheme: dark)').matches ? oneDark : oneLight;
   const match = /language-(\w+)/.exec(className || '');
 
   if (match === null) {
     return <React.Fragment />;
   }
 
-  return <SyntaxHighlighter children={String(content).replace(/\n$/, '')} style={theme} language={match[1]} />;
+  return (
+    <SyntaxHighlighter
+      children={String(content).replace(/\n$/, '')}
+      language={match[1]}
+      showLineNumbers={true}
+      useInlineStyles={false}
+      wrapLongLines={true}
+    />
+  );
 };
