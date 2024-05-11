c7912f8ffa2837e99580f7563ca7ad523c889485
0fprod <franp.dev@pm.me>
2024-05-11 05:58:18 +0100

    fix import type

--- src/components/posts/post/Post.tsx
+++ src/components/posts/post/Post.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Article } from './Post.styles';
-import { Post } from '@/models/Post';
+import type { Post } from '@/models/Post';
 import remarkGfm from 'remark-gfm';
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 import { Code } from '@/components/posts/post/code/Code';
